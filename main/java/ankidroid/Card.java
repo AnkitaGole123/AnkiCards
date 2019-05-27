9dbac5a5d0763e056911a38efc3ab33a37c98eae
Ankitagole <ankita17@navgurukul.org>
2019-05-27 12:00:13 +0530

    Adding cards

--- main/java/ankidroid/Card.java
+++ main/java/ankidroid/Card.java
@@ -1,2 +1,11 @@
 package ankidroid;
+public class Card {
+    private String frontDescription;
+    private String backDescription;
 
+
+    public Card(String frontDescription, String backDescription) {
+        this.frontDescription = frontDescription;
+        this.backDescription = backDescription;
+    }
+}
